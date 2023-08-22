7ad3416d691447f9f65b4dad45e1fea43215562b
TheKha01999 <thekha01999@gmail.com>
2023-08-22 21:18:33 +0700

    lan 2

--- resources/views/client/pages/hero-section.blade.php
+++ resources/views/client/pages/hero-section.blade.php
@@ -45,14 +45,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="hero__item set-bg" data-setbg="{{ asset('assets/client/img/hero/banner.jpg') }}">
-                    <div class="hero__text">
-                        <span>FRUIT FRESH</span>
-                        <h2>Vegetable <br />100% Organic</h2>
-                        <p>Free Pickup and Delivery Available</p>
-                        <a href="#" class="primary-btn">SHOP NOW</a>
-                    </div>
-                </div>
+                @yield('hero-home-banner')
             </div>
         </div>
     </div>
